44736776cbcbc550532fe0f8d993ec12eddc0bec
Markus-Ovaska <bhi028@gmyy.haaga-helia.fi>
2024-11-26 14:37:01 +0200

    book ja controlelr

--- src/main/java/bookstore/bookstore/web/BookController.java
+++ src/main/java/bookstore/bookstore/web/BookController.java
@@ -1,7 +1,19 @@
 package bookstore.bookstore.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+
 public class BookController {
 
-}
+    @RequestMapping("*")
+    @ResponseBody
+    public String index()
+    {
+        return "Bookstore";
+    }
+    
+    }
